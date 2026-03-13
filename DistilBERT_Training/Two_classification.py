db2f78e345e6ebabe7882a41e9679a1601d1714e
Misael Perez <misaelperez@mac.mynetworksettings.com>
2026-03-12 21:36:43 -0400

    add new things

--- DistilBERT_Training/Two_classification.py
+++ DistilBERT_Training/Two_classification.py
@@ -1,10 +1,10 @@
 from datasets import load_dataset
-from datasets import Dataset
 from transformers import (AutoTokenizer, AutoModel,AutoModelForSequenceClassification,
     Trainer, TrainingArguments)
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from transformers import DataCollatorWithPadding
 import pandas as pd
 import numpy as np
@@ -25,7 +25,7 @@ you need to use a GPU based model like BERT
 class two_TaskModel(nn.Module):
     #Let's build our constructor
     #let's put a place holder for the model_name
-    def __init__(self,model_name="Model you want"):
+    def __init__(self,model_name="roberta-base"):
         super(two_TaskModel,self).__init__()
         #load the headless Model
         self.encoder = AutoModel.from_pretrained(model_name)
@@ -44,22 +44,21 @@ class two_TaskModel(nn.Module):
             attention_mask=attention_mask
         )
         
-        cls_outputs= outputs.last_hidden_state[:,0,:]
+        cls_outputs= outputs.last_hidden_state[:,0]
         cls_output= self.dropout(cls_outputs)
         
         #The following lines of code will decide which head to use
         if task=="News":
             logits= self.real_or_fake(cls_output)
+            loss_fn= nn.CrossEntropyLoss()
         elif task == "evidence":
             logits = self.evidence_based(cls_output)
+            loss_fn = nn.CrossEntropyLoss(weight=class_weights)
         else:
             raise ValueError("Please select the right task")
-        
         loss=None
-        if labels is not None:
-            loss_fn = nn.CrossEntropyLoss()
-            loss = loss_fn(logits, labels)
         return {"loss": loss, "logits": logits}
+    def data_tokenizer(self,dataset,task,)
 #This class is to tokenizer the kaggle dataset used for distilBERT   
 class News_Datasets(Dataset):
     def __init__(self,texts,labels,tokenizer):
@@ -81,7 +80,8 @@ class News_Datasets(Dataset):
         return {
             "input_ids": encoding["input_ids"].squeeze(),
             "attention_mask": encoding["attention_mask"].squeeze(),
-            "labels": torch.tensor(self.labels[idx])
+            "labels": torch.tensor(self.labels[idx]),
+            "task":"News"
         }
 #class is to tokenizer the fever gold dataset
 class Evidence_Datasets(Dataset):
@@ -89,23 +89,65 @@ class Evidence_Datasets(Dataset):
         self.texts=dataset
         self.labels=labels
         self.tokenizer=tokenizer
+    def __len__(self):
+        return len(self.dataset)
 
-    def tokenization(claims):
+    def __getitem__(self,idx):
         evidences=[]
-        for e in claims["evidence"]:
+        for e in idx["evidence"]:
             if len(e)>0:
                 text=" ".join([ev[2] for ev in e])
             else:
                 text=""
             evidences.append(text)
-
-        return tokenizer(
-            claims["claim"],
-            evidences,
+        encoding= self.tokenizer(
+            self.dataset["claim"],
+            self.evidences,
             truncation=True,
             padding="max_length",
-            max_length=512
+            max_length=512,
+            return_tensors="pt"
+            
         )
+        return{
+            "input_ids": encoding["input_ids"].squeeze(),
+            "attention_mask": encoding["attention_mask"].squeeze(),
+            "labels": torch.tensor(self.labels[idx]),
+            "task":"evidence"
+        }
+# A function that will set the training arguments base on the task
+def get_Arguments(task):
+    if (task=="News"):
+        return TrainingArguments(
+        output_dir="news",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        gradient_accumulation_steps=4,
+        per_device_eval_batch_size=16,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+        )
+    if (task=="evidence"):
+        return TrainingArguments(
+        "evidence",
+        eval_strategy="steps",
+        eval_steps=1000,
+        learning_rate=2e-5,                 
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=256,     
+        num_train_epochs=3,                 
+        weight_decay=0.01,
+        logging_steps=500,   
+        save_strategy="steps",
+        save_steps=1000,                     
+        warmup_ratio=0.1,
+        load_best_model_at_end=True,
+        metric_for_best_model="macro_f1",
+        greater_is_better=True
+)
 #The following classes are for the compute metrics
 accuracy_metric= evaluate.load("accuracy")
 precision_metrics= evaluate.load("precision")
@@ -131,7 +173,7 @@ def new_compute_metrics(eval_pred):
 def evidence_compute_metrics(eval_preds):
     logits, labels=eval_preds
     predicts=np.argmax(logits, axis=-1)
-    acc=accuracy_metric.compute(predictions=predicts, references=labels)
+    acc=accuracy_metric.compute(predictions=predicts, references=labels)["accuracy"]
     macro_f1=f1_metric.compute(predictions=predicts, references=labels, average="macro")["f1"]    #macro since there are way more "SUPPORTS" then the other 2 labels
     weighted_f1=f1_metric.compute(predictions=predicts, references=labels, average="weighted")["f1"]
     per_class=f1_metric.compute(predictions=predicts, references=labels, average=None )["f1"]
@@ -153,7 +195,7 @@ To organize our datasets and the way we are going to modify them, we are going t
 train_data= pd.read_csv("train.csv")
 eval_data=pd.read_csv("eval.csv")
 test_data=pd.read_csv("test.csv")
-tokenizer= AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
 id2label= {0: "Fake", 1:"Real"}
 
 """The following part of the code is to prepare for the tokenization of the fever gold evidence data"""
@@ -169,12 +211,11 @@ fever_train_dataset=feverDataset["train"]
 fever_validation_dataset=feverDataset["validation"] # will use to evaluate model
 fever_test_dataset=feverDataset["test"] #will use to test the model later
 
-supports_count=fever_train_dataset["labels"].count(0)
-refutes_count=fever_train_dataset["labels"].count(1)
-nei_count=fever_train_dataset["labels"].count(2)
-checkpoint="roberta-base"
-tokenizer=AutoTokenizer.from_pretrained(checkpoint)
-fever_train_dataset=fever_train_dataset.select_columns(["input_ids", "attention_mask", "label"])
+supports_count=fever_train_dataset["label"].count(0)
+refutes_count=fever_train_dataset["label"].count(1)
+nei_count=fever_train_dataset["label"].count(2)
+evidence_tokenizer=AutoTokenizer.from_pretrained("roberta-base")
+class_weights=torch.tensor([1.0/supports_count, 1.0/refutes_count, 1.0/nei_count])  
 
 
 
