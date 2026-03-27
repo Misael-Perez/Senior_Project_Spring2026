02d6775ed8c115a1e62ed1067b49882d0e39e106
Misael Perez <misaelperez@mac.mynetworksettings.com>
2026-03-27 10:08:12 -0400

    code for combined training

--- DistilBERT_Training/Two_classification.py
+++ DistilBERT_Training/Two_classification.py
@@ -247,12 +247,6 @@ news_trainer=Trainer(
 model.current_task="News"
 news_trainer.train()
 tokenizer.save_pretrained("news_tokenizer/")
-for param in model.encoder.parameters():
-    param.requires_grad = False
-
-for name, param in model.named_parameters():
-    if param.requires_grad:
-        print("TRAINING:", name)
 
 class myTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
@@ -288,19 +282,7 @@ print("---------------------")
 print(testPredictions.metrics)
 # labels of testPredictions: 'count', 'index', 'label_ids', 'metrics', 'predictions'
 
-actual=testPredictions.label_ids
-preds=np.argmax(testPredictions.predictions, axis=1)
-
-df=pd.DataFrame({
-    "actual": actual,
-    "prediction": preds,
-    "result": (actual==preds)
-    })
-df.to_csv("Freeze_results_2.csv", index=False)
-print("METRICS: ", testPredictions.metrics)
-#print(df.head())
-
-with open('metrics_freeze2.txt', 'w') as f:
+with open('metrics_SEQ_1e3.txt', 'w') as f:
     f.write(json.dumps(testPredictions.metrics, indent=4))
 
 """Now that we have trained the model, we will begin to save. We can't save it the regular way, so
@@ -319,7 +301,7 @@ Which is class above."""
 torch.save({
     "Two_task_Model_1": model.state_dict(),
     "class_weights": class_weights
-}, "TwoTask_Model_1_full_SEQ_freeze_2.pt")
+}, "TwoTask_Model_1_full_SEQ_1e3.pt")
 
 
 
